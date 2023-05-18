4c0c06e706b35b11ea99d484c8da0e4cd53c3118
Karsten <Karsten1974git@gmx.de>
2023-05-18 16:32:56 +0200

    Alte Sachen wie Komponente gelöscht

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdressenComponent } from './adressen/adressen.component';
-import { HomeComponent } from './home/home.component';
 import { BrancheComponent } from './pages/branche-bearbeiten/branche.component';
-import { VerbindungsartComponent } from './pages/verbindungsart-bearbeiten/verbindungsart.component';
 import {ContactVerwaltenComponent} from "./pages/contact-verwalten/contact-verwalten.component";
 import {ContactBearbeitenComponent} from "./pages/contact-bearbeiten/contact-bearbeiten.component";
 
@@ -12,10 +9,9 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
+  },{
     path: 'home',
-    component: HomeComponent
+    component: ContactVerwaltenComponent
   },{
     path: 'verwalten',
     component: ContactVerwaltenComponent
@@ -25,31 +21,13 @@ const routes: Routes = [
   },{
     path: 'bearbeiten/:contactUUID',
     component: ContactBearbeitenComponent
-  }, {
-    path: 'adressen',
-    component: AdressenComponent
-  },
-  {
-    path: 'adressen/:contactUUID',
-    component: AdressenComponent
-  },
-  {
+  },{
     path: 'branchen',
     component: BrancheComponent
-  },
-  {
+  },{
     path: 'branche/:brancheUUID',
     component: BrancheComponent
-  },
-  {
-    path: 'verbindungsart',
-    component: VerbindungsartComponent
-  },
-  {
-    path: 'verbindungsart/:verbindungsartUUID',
-    component: VerbindungsartComponent
-  },
-
+  }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,23 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AdressePersonComponent } from './adressen/adresse-person/adresse-person.component';
-import { AdresseTodeslisteComponent } from './adressen/adresse-todesliste/adresse-todesliste.component';
-import { AdressenComponent } from './adressen/adressen.component';
-import { AdressePersonZusatzComponent } from './adressen/adresse-person-zusatz/adresse-person-zusatz.component';
-import { AdresseAdressenComponent } from './adressen/adresse-adressen/adresse-adressen.component';
-import { AdresseVerbindungsdatenComponent } from './adressen/adresse-verbindungsdaten/adresse-verbindungsdaten.component';
-import { AdresseBeziehungenComponent } from './adressen/adresse-beziehungen/adresse-beziehungen.component';
-import { AdresseSuchenComponent } from './adressen/adresse-suchen/adresse-suchen.component';
-import { AdresseDialogNeuComponent } from './adressen/adresse-dialog-neu/adresse-dialog-neu.component';
 import { BrancheComponent } from './pages/branche-bearbeiten/branche.component';
 import { BrancheBearbeitenComponent } from './pages/branche-bearbeiten/components/branche-bearbeiten/branche-bearbeiten.component';
 import { BrancheListeComponent } from './pages/branche-bearbeiten/components/branche-liste/branche-liste.component';
-import { VerbindungsartBearbeitenComponent } from './pages/verbindungsart-bearbeiten/components/verbindungsart-bearbeiten/verbindungsart-bearbeiten.component';
-import { VerbindungsartListeComponent } from './pages/verbindungsart-bearbeiten/components/verbindungsart-liste/verbindungsart-liste.component';
-import { VerbindungsartComponent } from './pages/verbindungsart-bearbeiten/verbindungsart.component';
 import { ContactVerwaltenComponent } from './pages/contact-verwalten/contact-verwalten.component';
 import { ContactDialogNeuComponent } from './pages/contact-verwalten/components/contact-dialog-neu/contact-dialog-neu.component';
 import { ContactDialogAdresseComponent } from './pages/contact-verwalten/components/contact-dialog-adresse/contact-dialog-adresse.component';
@@ -34,23 +20,9 @@ import { ContactZusatzComponent } from './pages/contact-bearbeiten/components/co
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    AdressePersonComponent,
-    AdresseTodeslisteComponent,
-    AdressenComponent,
-    AdressePersonZusatzComponent,
-    AdresseAdressenComponent,
-    AdresseVerbindungsdatenComponent,
-    AdresseBeziehungenComponent,
-    AdresseSuchenComponent,
-    AdresseDialogNeuComponent,
     BrancheComponent,
     BrancheBearbeitenComponent,
     BrancheListeComponent,
-    VerbindungsartBearbeitenComponent,
-    VerbindungsartListeComponent,
-    VerbindungsartComponent,
     ContactVerwaltenComponent,
     ContactDialogNeuComponent,
     ContactDialogAdresseComponent,
