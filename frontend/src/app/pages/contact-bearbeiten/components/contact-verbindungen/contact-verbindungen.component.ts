32b40afb8eaea7b72e442a1af7baeb1a295786a6
Karsten <Karsten1974git@gmx.de>
2023-11-17 18:06:17 +0100

    Verbindungen als Liste editierbar

--- frontend/src/app/pages/contact-bearbeiten/components/contact-verbindungen/contact-verbindungen.component.ts
+++ frontend/src/app/pages/contact-bearbeiten/components/contact-verbindungen/contact-verbindungen.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {ContactDto} from "../../../../backend-api/models/contact-dto";
 import {VerbindungDto} from "../../../../backend-api/models/verbindung-dto";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {ContactService} from "../../../../shared/service/contact.service";
 
 @Component({
@@ -9,12 +9,22 @@ import {ContactService} from "../../../../shared/service/contact.service";
   templateUrl: './contact-verbindungen.component.html',
   styleUrls: ['./contact-verbindungen.component.css']
 })
-export class ContactVerbindungenComponent implements OnInit {
-  verbindungForm: FormGroup = this.fb.group({
-    telefon: [''],
-    mobil: [''],
-    email: ['']
-  });
+export class ContactVerbindungenComponent implements OnInit, OnChanges {
+  verbindungsdatenArten: { verbindungsart: string; bezeichnung: string }[] = [
+  {
+    verbindungsart: 'TELEFON',
+    bezeichnung: 'Tel'
+  },
+  {
+    verbindungsart: 'MOBIL',
+    bezeichnung: 'mobil'
+  },
+  {
+    verbindungsart: 'EMAIL',
+    bezeichnung: 'eMail'
+  }
+  ];
+  verbindungsdatenForm: FormGroup = new FormGroup('');
   verbindungenEdit = false;
   @Input() contact : ContactDto = {
     version: 0,
@@ -25,16 +35,19 @@ export class ContactVerbindungenComponent implements OnInit {
   constructor(private fb: FormBuilder, private cs: ContactService) { }
 
   ngOnInit(): void {
+    let verbindungsdatenFormArray = new FormArray<FormGroup>([]);
+    this.verbindungsdatenForm = new FormGroup({
+      'verbindungsdaten': verbindungsdatenFormArray
+    });
+  }
+
+  ngOnChanges() {
+    if (this.contact.id) {
+      this.loadVebindungsdaten();
+    }
   }
 
   onBearbeiten() {
-    let verbindungen = {
-      'telefon': this.verbindungDaten(this.contact, 'TELEFON'),
-      'mobil': this.verbindungDaten(this.contact, 'MOBIL'),
-      'email': this.verbindungDaten(this.contact, 'EMAIL')
-    }
-
-    this.verbindungForm.patchValue(verbindungen);
     this.verbindungenEdit = true;
   }
 
@@ -51,30 +64,13 @@ export class ContactVerbindungenComponent implements OnInit {
     return "";
   }
 
-  setzeVerbindungDaten(verb: Array<VerbindungDto>, verbArt: 'TELEFON' | 'MOBIL' | 'EMAIL', wert?: string) {
-    let bFound = false;
-    for (let vb of verb) {
-      if (vb.verbindungsart === verbArt) {
-        vb.verbindungsdaten = wert;
-        bFound = true;
-      }
-    }
-
-    if (!bFound) {
-      verb.push({
-        'id': '',
-        'verbindungsart': verbArt,
-        'verbindungsdaten': wert
-      });
-    }
-  }
-
   onCancel() {
     this.verbindungenEdit = false;
   }
 
   onSpeichern() {
-    const formValue = this.verbindungForm.value;
+    const formValue = this.verbindungsdatenForm.value;
+    let verbindungsdatenFormArray = formValue['verbindungsdaten'];
 
     this.cs.getContact(this.contact.id).subscribe(res => {
 
@@ -94,19 +90,30 @@ export class ContactVerbindungenComponent implements OnInit {
         'verbindungen': res.verbindungen
       };
 
-      let verbindungen = saveContact.verbindungen;
+      let verbindungen = new Array<VerbindungDto>;
 
-      if (formValue['telefon'] != '') {
-        if (verbindungen) this.setzeVerbindungDaten(verbindungen, 'TELEFON', formValue['telefon']);
+      for (let frmGrp of verbindungsdatenFormArray) {
+
+        let vb :VerbindungDto = {
+          id:frmGrp['id'],
+          reihenfolge: frmGrp['reihenfolge'],
+          version: frmGrp['version'],
+          verbindungsdaten: frmGrp['verbindungsdaten'],
+          verbindungsart: 'TELEFON'
+        };
+
+        if (frmGrp['verbindungsart'].verbindungsart === 'TELEFON') {
+          vb.verbindungsart = 'TELEFON';
+        } else if (frmGrp['verbindungsart'].verbindungsart === 'MOBIL') {
+          vb.verbindungsart = 'MOBIL';
+        } if (frmGrp['verbindungsart'].verbindungsart === 'EMAIL') {
+          vb.verbindungsart = 'EMAIL';
+        }
+
+        verbindungen.push(vb);
       }
 
-      if (formValue['mobil'] != '') {
-        if (verbindungen) this.setzeVerbindungDaten(verbindungen, 'MOBIL', formValue['mobil']);
-      }
-
-      if (formValue['email'] != '') {
-        if (verbindungen) this.setzeVerbindungDaten(verbindungen, 'EMAIL', formValue['email']);
-      }
+      saveContact.verbindungen = verbindungen;
 
       this.cs.updateContact(saveContact).subscribe( res => {
         this.verbindungenEdit = false;
@@ -115,6 +122,55 @@ export class ContactVerbindungenComponent implements OnInit {
 
     });
 
+  }
+
+  private loadVebindungsdaten() {
+    this.cs.getContact(this.contact.id).subscribe(res=> {
+      let verbindungsdaten: VerbindungDto[] | undefined = res.verbindungen;
+
+      /* Verbindungsdaten bestehen aus Zeilen mit jeweils 2 Controls
+       * Die Zeilen (Array) werden als >verbindungsdaten< in einer FormGroup abgelegt.
+       */
+      let verbindungsdatenFormArray = new FormArray<FormGroup>([]);
+
+      if (verbindungsdaten != undefined) {
+        let verbindungsartItem;
+        for (let vdItem of verbindungsdaten) {
+
+          // passende Verbindunsdatenart zu Verbindungsdaten suchen
+          let bFound = false;
+          for (let vdaItem of this.verbindungsdatenArten) {
+            if (vdaItem.verbindungsart === vdItem.verbindungsart) {
+              verbindungsartItem = vdaItem;
+              bFound = true;
+            }
+          }
+
+          if (!bFound) {
+            verbindungsartItem = this.verbindungsdatenArten[0];
+          }
+
+          verbindungsdatenFormArray.push(
+            new FormGroup({
+              'verbindungsart': new FormControl(verbindungsartItem),
+              'verbindungsdaten': new FormControl(vdItem.verbindungsdaten),
+              'id': new FormControl(vdItem.id),
+              'reihenfolge': new FormControl(vdItem.reihenfolge),
+              'version': new FormControl(vdItem.version)
+            })
+          );
+        }
+      }
+
+      this.verbindungsdatenForm = new FormGroup({
+        'verbindungsdaten': verbindungsdatenFormArray
+      });
+
+    });
+  }
+
+  get controls() {
+    return (<FormArray>this.verbindungsdatenForm.get('verbindungsdaten')).controls;
   }
 
 }
